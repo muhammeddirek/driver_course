10158afb2856b313f10716d820e9d0313c6f1615
muhammeddirek <muhammeddirek34@gmail.com>
2024-06-03 21:51:09 +0300

    uds

--- app/personel-kayit-ol/page.jsx
+++ app/personel-kayit-ol/page.jsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
 export default  function page() {
